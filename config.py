2a75c8c4d26fcafb2158806d047b551d478fad5e
Bjørnar Myrheim <45852703+Myrheimb@users.noreply.github.com>
2019-02-04 14:23:10 +0100

    Update and rename travelData.py to config.py
    Renamed from travelData to config and changed from using variables to using a dictionary.

---
+++ config.py
@@ -0,0 +1,38 @@
+config = {
+    # Point to your own Excel workbook inside the ''.
+    'excelWorkBook' : '2018.xlsx',
+    # Fill in the addresses you normally drive to and from.
+    'Home' : '96 Prairie Drive, Zionsville, IN 46077',
+    'Work1' : '0386 Myers St., Williamsport, PA 17701',
+    'Work2' : '9362 W. Ivy Drive, Westwood, NJ 07675',
+    'Work3' : '8703 Prospect Ave., Savannah, GA 31404',
+    'Work4' : '8017 Berkshire Dr., Jackson, NJ 08527',
+    'Work5' : '8337 W. Windsor St., Lombard, IL 60148',
+    # Fill in the names of your frequently visited work places.
+    'wpWork1' : 'Location 1',
+    'wpWork2' : 'Location 2',
+    'wpWork3' : 'Location 3',
+    'wpWork4' : 'Location 4',
+    'wpWork5' : 'Location 5',
+    # Fill in the distance between locations.
+    # Spend some time doing this once, save a lot of time later!
+    'distHomeWork1' : 19,
+    'distHomeWork3' : 33,
+    'distHomeWork2' : 38.5,
+    'distHomeWork4' : 23,
+    'distHomeWork5' : 52.5,
+    'distWork1Work2' : 29,
+    'distWork1Work3' : 19,
+    'distWork1Work4' : 19,
+    'distWork1Work5' : 43,
+    'distWork2Work3' : 21,
+    'distWork2Work4' : 18.5,
+    'distWork2Work5' : 20,
+    'distWork3Work4' : 27,
+    'distWork3Work5' : 34,
+    'distWork4Work5' : 36.5,
+    # Fill in your desired button text.
+    'retrieveInput' : 'Submit comment',
+    'writeToExcel' : 'Save and new',
+    'writeToExcelClose' : 'Save and close'
+}
